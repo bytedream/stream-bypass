81da6600e6c030aeb6b9541de68bf85751c7813e
bytedream <bytedream@protonmail.com>
2022-07-22 13:55:03 +0200

    Fix error message if video cannot be loaded

--- src/ui/player/player.ts
+++ src/ui/player/player.ts
@@ -3,7 +3,7 @@ import {Match, matches, Reliability} from "../../match/matches";
 import Hls from "hls.js";
 
 function show_message(message: string) {
-    document.getElementById('message').innerText = message
+    document.getElementById('message').innerHTML = message
     document.getElementById('message-container').hidden = false
     document.getElementById('video').hidden = true
 }
@@ -48,13 +48,13 @@ async function play_hls(url: string, match: Match) {
                     message = `The reliability for this domain is normal, errors like this can occur but are not very common. Try to refresh the page`
                     break
                 case Reliability.HIGH:
-                    message = `The reliability for this domains is high, errors like this are very unlikely to happen.
-                    Try to refresh the page and if the error still exists you might want to open a new issue <a href="https://github.com/ByteDream/stream-bypass/issues/new">here</a>.
-                    When you're using <a href="https://www.torproject.org/">Tor</a> such errors have a slight chance to occur more often,
+                    message = `The reliability for this domains is high, errors like this are very unlikely to happen.<br>
+                    Try to refresh the page and if the error still exists you might want to open a new issue <a href="https://github.com/ByteDream/stream-bypass/issues/new">here</a>.<br>
+                    When you're using <a href="https://www.torproject.org/">Tor</a>, such errors have a slight chance to occur more often,
                     so if this is the case just try to reload the page and see if it's working then`
                     break
             }
-            show_message(`Could not load HLS video. ${message}`)
+            show_message(`Could not load video. ${message}`)
         }
     } else {
         show_message('Failed to play m3u8 video (hls is not supported). Try again or create a new issue <a href="https://github.com/ByteDream/stream-bypass/issues/new">here</a>')
