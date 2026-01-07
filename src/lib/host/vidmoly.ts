67dbb9c6851272ba5c48070ed57e944f041bb3f9
bytedream <me@bytedream.dev>
2026-01-08 00:50:00 +0100

    fix vidmoly

--- src/lib/host/vidmoly.ts
+++ src/lib/host/vidmoly.ts
@@ -3,7 +3,11 @@ import { HostMatchType, type Host } from '@/lib/host';
 export default {
 	name: 'Vidmoly',
 	id: 'vidmoly',
-	domains: ['vidmoly.me', 'vidmoly.net', 'vidmoly.to'],
+	// vidmoly has multiple domains, but regardless of the domain, the actual videos are loaded via an iframe which
+	// always points to vidmoly.net. the "outer" vidmoly site also has a link to some video, which, would
+	// be preferred to use, as the whole site could be replaced by the native video player instead of just the iframe,
+	// but said link doesn't always point the same video as the iframe
+	domains: ['vidmoly.net'],
 	regex: [/(?<=file:").+\.m3u8.*(?=")/gm],
 
 	match: async function (match: RegExpMatchArray) {
